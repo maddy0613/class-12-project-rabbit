e971336c72f71bf4f369977660385da73e333bfa
maddy0613 <99407497+maddy0613@users.noreply.github.com>
2022-06-14 18:47:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,15 +46,15 @@ function draw() {
 var select_sprites = Math.round(random(1,3));
 
   
-   if (frameCount % 50 == 0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-     }else {
-       createRed();
-     }
-   }
+  // if (frameCount % 50 == 0) {
+    // if (select_sprites == 1) {
+      // createApples();
+     //} else if (select_sprites == 2) {
+       //createOrange();
+    // }else {
+      // createRed();
+  //   }
+  // }
 
   // if (frameCount % 80 == 0) {
   //   if (select_sprites == 1) {
@@ -74,15 +74,15 @@ var select_sprites = Math.round(random(1,3));
   //   }
   // }
 
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+   }
 
 
 
